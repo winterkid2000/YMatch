fbe682bb56565323ca9c6646992c7fb271507d8c
winterkid <137682088+winterkid2000@users.noreply.github.com>
2025-04-23 13:11:18 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
 from .database import Base, engine
 from .api import ride, match
+from .views import home 
 
 app = FastAPI()
+
 Base.metadata.create_all(bind=engine)
+
 app.include_router(ride.router, prefix="/api")
 app.include_router(match.router, prefix="/api")
+
+app.include_router(home.router)   
